dcaff79e490802a1b4c489431426756bb2771a19
Ngoc Nguyen <rubj712@gmail.com>
2018-06-12 23:09:54 -0700

    new change

---
+++ Assignment2/Assignment2/main.cpp
@@ -0,0 +1,15 @@
+/*
+ Name: Ngoc Nguyen
+ SID#: 0472702
+ Date: 06/12/2018
+ Asignment#: 2
+ Description: 
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
